d36e500b236823f51f6b6cac140934d3286d0134
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2024-10-20 00:28:41 -0400

    basic router

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+    res.status(200).send("Working!");
+});
+
+
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
