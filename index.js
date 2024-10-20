1cae9ed186064b2b1b2005bd6541c06ac6344e0d
kylefwalters <41218512+kylefwalters@users.noreply.github.com>
2024-10-20 09:28:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get("/", (req, res) => {
-    res.status(200).send("Updated?");
+    res.status(200).send("Should automatically update");
 });
 
 
